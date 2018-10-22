959ca1a34637d46d5aa13c9425d06727f76ed9a3
Muhammed Albarmawi <mohammad.albarmawi@outlook.com>
2018-10-22 23:09:14 +0300

    add lesson 24 content , from validation

--- src/app/lessons/lesson24/lesson24.component.ts
+++ src/app/lessons/lesson24/lesson24.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-lesson24',
-  templateUrl: './lesson24.component.html',
-  styleUrls: ['./lesson24.component.scss']
+  selector: "app-lesson24",
+  templateUrl: "./lesson24.component.html",
+  styleUrls: ["./lesson24.component.scss"]
 })
 export class Lesson24Component implements OnInit {
+  public form: FormGroup;
+  public selectedValue: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ["", [Validators.required]],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      address: [""]
+    });
   }
 
+  ngOnInit() {}
+
+  public save() {
+    console.log(this.form.value);
+    this.selectedValue = this.form.value;
+  }
+
+  public reset(): void {
+    this.form.reset();
+    console.log(this.form.value);
+    this.selectedValue = null;
+  }
+
+  public get(name: string): AbstractControl {
+    return this.form.get(name);
+  }
 }
