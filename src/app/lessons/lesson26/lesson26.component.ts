5056c311a9e19a4a15744343d45f5e69395c5397
Muhammed Albarmawi <mohammad.albarmawi@outlook.com>
2018-10-23 22:39:51 +0300

    add lesson 26 content (sub form group)

--- src/app/lessons/lesson26/lesson26.component.ts
+++ src/app/lessons/lesson26/lesson26.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-lesson26',
-  templateUrl: './lesson26.component.html',
-  styleUrls: ['./lesson26.component.scss']
+  selector: "app-lesson26",
+  templateUrl: "./lesson26.component.html",
+  styleUrls: ["./lesson26.component.scss"]
 })
 export class Lesson26Component implements OnInit {
+  public form: FormGroup;
+  public selectedValue: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ["", [Validators.required]],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      address: fb.group({
+        country: ["", Validators.required],
+        city: ["", Validators.required],
+        street: null,
+        zipCode: ["", [Validators.minLength(4), Validators.maxLength(4)]]
+      })
+    });
   }
 
+  ngOnInit() {}
+
+  public save() {
+    console.log(this.form.value);
+    this.selectedValue = this.form.value;
+  }
+
+  public reset(): void {
+    this.form.reset();
+    console.log(this.form.value);
+    this.selectedValue = null;
+  }
+
+  public get(name: string): AbstractControl {
+    return this.form.get(name);
+  }
+
+  public get address(): FormGroup {
+    return this.get("address") as FormGroup;
+  }
 }
