55ab1d6cfd385b3aab3c698f30957efdf3a5d29c
Muhammed Albarmawi <mohammad.albarmawi@outlook.com>
2018-10-24 13:55:38 +0300

    add lesson 29 content (make http request)

--- src/app/lessons/lesson29/lesson29.component.ts
+++ src/app/lessons/lesson29/lesson29.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-lesson29',
-  templateUrl: './lesson29.component.html',
-  styleUrls: ['./lesson29.component.scss']
+  selector: "app-lesson29",
+  templateUrl: "./lesson29.component.html",
+  styleUrls: ["./lesson29.component.scss"]
 })
 export class Lesson29Component implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public getData(): void {
+    this.http
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .subscribe(result => console.log(result));
+    // this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(console.log);
   }
 
+  public sendData(): void {
+    let data = {
+      userName: "user01",
+      message: "any message"
+    };
+    this.http
+      .post("https://jsonplaceholder.typicode.com/posts", JSON.stringify(data))
+      .subscribe(result => console.log(result));
+  }
 }
--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -1,45 +1,53 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Lesson0Component } from './lesson0/lesson0.component';
-import { Lesson1Component } from './lesson1/lesson1.component';
-import { Lesson2Component } from './lesson2/lesson2.component';
-import { Lesson3Component } from './lesson3/lesson3.component';
-import { Lesson4Component } from './lesson4/lesson4.component';
-import { Lesson5Component } from './lesson5/lesson5.component';
-import { Lesson6Component } from './lesson6/lesson6.component';
-import { Lesson7Component } from './lesson7/lesson7.component';
-import { Lesson8Component } from './lesson8/lesson8.component';
-import { Lesson9Component } from './lesson9/lesson9.component';
-import { Lesson10Component } from './lesson10/lesson10.component';
-import { Lesson11Component } from './lesson11/lesson11.component';
-import { LessonRouteModule } from './lessons.routing';
-import { InstallComponent } from './install/install.component';
-import { Lesson12Component } from './lesson12/lesson12.component';
-import { Lesson13Component } from './lesson13/lesson13.component';
-import { Lesson14Component } from './lesson14/lesson14.component';
-import { ResourceComponent } from './resource/resource.component';
-import { MaterialModule } from '../modules/material.module';
-import { Lesson15Component } from './lesson15/lesson15.component';
-import { Lesson16Component } from './lesson16/lesson16.component';
-import { Lesson17Component } from './lesson17/lesson17.component';
-import { Lesson18Component } from './lesson18/lesson18.component';
-import { Lesson19Component } from './lesson19/lesson19.component';
-import { IndexComponent } from './index/index.component';
-import { Lesson20Component } from './lesson20/lesson20.component';
-import { Lesson21Component } from './lesson21/lesson21.component';
-import { Lesson22Component } from './lesson22/lesson22.component';
-import { Lesson23Component } from './lesson23/lesson23.component';
-import { Lesson24Component } from './lesson24/lesson24.component';
-import { Lesson25Component } from './lesson25/lesson25.component';
-import { Lesson26Component } from './lesson26/lesson26.component';
-import { Lesson27Component } from './lesson27/lesson27.component';
-import { Lesson28Component } from './lesson28/lesson28.component';
-import { Lesson29Component } from './lesson29/lesson29.component';
-import { Lesson30Component } from './lesson30/lesson30.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { Lesson0Component } from "./lesson0/lesson0.component";
+import { Lesson1Component } from "./lesson1/lesson1.component";
+import { Lesson2Component } from "./lesson2/lesson2.component";
+import { Lesson3Component } from "./lesson3/lesson3.component";
+import { Lesson4Component } from "./lesson4/lesson4.component";
+import { Lesson5Component } from "./lesson5/lesson5.component";
+import { Lesson6Component } from "./lesson6/lesson6.component";
+import { Lesson7Component } from "./lesson7/lesson7.component";
+import { Lesson8Component } from "./lesson8/lesson8.component";
+import { Lesson9Component } from "./lesson9/lesson9.component";
+import { Lesson10Component } from "./lesson10/lesson10.component";
+import { Lesson11Component } from "./lesson11/lesson11.component";
+import { LessonRouteModule } from "./lessons.routing";
+import { InstallComponent } from "./install/install.component";
+import { Lesson12Component } from "./lesson12/lesson12.component";
+import { Lesson13Component } from "./lesson13/lesson13.component";
+import { Lesson14Component } from "./lesson14/lesson14.component";
+import { ResourceComponent } from "./resource/resource.component";
+import { MaterialModule } from "../modules/material.module";
+import { Lesson15Component } from "./lesson15/lesson15.component";
+import { Lesson16Component } from "./lesson16/lesson16.component";
+import { Lesson17Component } from "./lesson17/lesson17.component";
+import { Lesson18Component } from "./lesson18/lesson18.component";
+import { Lesson19Component } from "./lesson19/lesson19.component";
+import { IndexComponent } from "./index/index.component";
+import { Lesson20Component } from "./lesson20/lesson20.component";
+import { Lesson21Component } from "./lesson21/lesson21.component";
+import { Lesson22Component } from "./lesson22/lesson22.component";
+import { Lesson23Component } from "./lesson23/lesson23.component";
+import { Lesson24Component } from "./lesson24/lesson24.component";
+import { Lesson25Component } from "./lesson25/lesson25.component";
+import { Lesson26Component } from "./lesson26/lesson26.component";
+import { Lesson27Component } from "./lesson27/lesson27.component";
+import { Lesson28Component } from "./lesson28/lesson28.component";
+import { Lesson29Component } from "./lesson29/lesson29.component";
+import { Lesson30Component } from "./lesson30/lesson30.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, LessonRouteModule, MaterialModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LessonRouteModule,
+    MaterialModule,
+    HttpClientModule
+  ],
   declarations: [
     InstallComponent,
     IndexComponent,
@@ -77,6 +85,6 @@ import { Lesson30Component } from './lesson30/lesson30.component';
     Lesson30Component
   ]
 })
-export class LessonsModule { }
+export class LessonsModule {}
 
-export { routes as lessonRoutesList } from './lessons.routing';
+export { routes as lessonRoutesList } from "./lessons.routing";
