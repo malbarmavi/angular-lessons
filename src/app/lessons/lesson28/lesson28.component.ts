524fb02c50cd7826f47228fedc6175476222e106
Muhammed Albarmawi <mohammad.albarmawi@outlook.com>
2018-10-24 13:37:05 +0300

    add lesson 28 content (form control value changes)

--- src/app/lessons/lesson28/lesson28.component.ts
+++ src/app/lessons/lesson28/lesson28.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-lesson28',
-  templateUrl: './lesson28.component.html',
-  styleUrls: ['./lesson28.component.scss']
+  selector: "app-lesson28",
+  templateUrl: "./lesson28.component.html",
+  styleUrls: ["./lesson28.component.scss"]
 })
 export class Lesson28Component implements OnInit {
+  public form: FormGroup;
+  public selectedValue: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ["", [Validators.required]],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      userType: [""]
+    });
   }
 
+  ngOnInit() {
+    this.form.get("userType").valueChanges.subscribe(value => {
+      if (value == "user") {
+        this.form.get("email").enable();
+      } else {
+        this.form.get("email").disable();
+        this.form.get("email").setValue(null);
+      }
+    });
+  }
+
+  public save() {
+    console.log(this.form.value);
+    this.selectedValue = this.form.value;
+  }
+
+  public reset(): void {
+    this.form.reset({ userType: "user" });
+    console.log(this.form.value);
+    this.selectedValue = null;
+  }
+
+  public get(name: string): AbstractControl {
+    return this.form.get(name);
+  }
 }
