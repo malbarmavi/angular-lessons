507f80eb475bc4133c320deef1b5821c468cc2b3
Muhammed Albarmawi <mohammad.albarmawi@outlook.com>
2018-10-24 16:35:56 +0300

    add lesson 30 content (pipes)

--- src/app/lessons/lesson30/lesson30.component.ts
+++ src/app/lessons/lesson30/lesson30.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-lesson30',
-  templateUrl: './lesson30.component.html',
-  styleUrls: ['./lesson30.component.scss']
+  selector: "app-lesson30",
+  templateUrl: "./lesson30.component.html",
+  styleUrls: ["./lesson30.component.scss"]
 })
 export class Lesson30Component implements OnInit {
+  public number = Math.PI;
+  public currentDate = new Date();
+  public dateForms = [
+    "short",
+    "medium",
+    "long",
+    "full",
+    "shortDate",
+    "mediumDate",
+    "longDate",
+    "fullDate",
+    "shortTime",
+    "mediumTime",
+    "longTime",
+    "fullTime"
+  ];
 
-  constructor() { }
+  public user: any = {
+    id: 1,
+    name: "name01",
+    lastName: "lastName01",
+    address: {
+      country: "country name",
+      city: "city name",
+      street: "street name"
+    },
+    age: 35,
+    email: "example@example.com"
+  };
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
